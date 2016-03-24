fb35ea1625899448fa4ac326709baa14bc4740f3
oakes <zsoakes@gmail.com>
2016-03-24 11:12:17 -0400

    Start server with / route

--- src/microblog_clojure/core.clj
+++ src/microblog_clojure/core.clj
@@ -1,5 +1,15 @@
 (ns microblog-clojure.core
+  (:require [compojure.core :as c]
+            [ring.adapter.jetty :as j])
   (:gen-class))
 
+(c/defroutes app
+  (c/GET "/" request
+    "Hello, world!"))
+
+(defonce server (atom nil))
+
 (defn -main []
-  (println "Hello, World!"))
+  (when @server
+    (.stop @server))
+  (reset! server (j/run-jetty app {:port 3000 :join? false})))
